b9229aa7b1de68f0818bbc724668150dcfc42bcc
Kyuwon-Choi <kyuwon30@naver.com>
2024-10-14 16:56:08 +0900

    add: 초기 커밋

---
+++ Sonsuje/src/main/java/com/example/sonsuje/SonsujeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonsuje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonsujeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonsujeApplication.class, args);
+    }
+
+}
---
+++ Sonsuje/src/test/java/com/example/sonsuje/SonsujeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonsuje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonsujeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
